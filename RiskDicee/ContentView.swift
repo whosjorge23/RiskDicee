1b76a961d54f944f20fd5d94d6c23c0589910d01
Giorgio Giannotta <jorge_upa@msn.com>
2021-01-18 19:27:21 +0100

    Initial Commit

---
+++ RiskDicee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RiskDicee
+//
+//  Created by Jorge Giannotta on 18/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RiskDicee/RiskDiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiskDiceeApp.swift
+//  RiskDicee
+//
+//  Created by Jorge Giannotta on 18/01/21.
+//
+
+import SwiftUI
+
+@main
+struct RiskDiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
